0f38846d43694496508f5b7d9974fc3caac34255
willemharmse <harmse688@gmail.com>
2024-08-13 11:57:51 +0200

    Put the port in a env file as well as started with the connecting of the database

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,8 +1,11 @@
 import express from 'express';
+import db from './db.mjs';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`);
