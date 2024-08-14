61e6fedd003eff772ca79d086bf7ccbdde6eb8a8
willemharmse <harmse688@gmail.com>
2024-08-14 09:00:33 +0200

    Created the first route

---
+++ backend/src/routes/auth.mjs
@@ -0,0 +1,20 @@
+import express from 'express';
+import passport from '../auth.mjs';
+
+const router = express.Router();
+
+router.get('/login', passport.authenticate('openidconnect'));
+
+router.get('/callback', 
+  passport.authenticate('openidconnect', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/dashboard');
+  }
+);
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+export default router;
