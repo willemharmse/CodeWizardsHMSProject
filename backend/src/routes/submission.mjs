d60f6341db24b20c384a66817ae7959b2d90e7c2
Brian <anderson.bri1909@gmail.com>
2024-08-21 09:45:38 +0200

    Created Submission endpoint

---
+++ backend/src/routes/submission.mjs
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/api/submission', functon(req, res){
+
+    res.send('No submission created yet');
+
+});
+
+export default router;
