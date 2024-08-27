2293d93b9ed61fb5307913ac8c8a63f9fe4e3db8
Willem Harmse <133529442+willemharmse@users.noreply.github.com>
2024-08-27 10:30:22 +0200

    Willem (#5)
    * Assignments datamodel created
    
    * Completed the submission model
    
    * created file model and fixed errors with submission model
    
    * Added an env file and disabled tracking for it
    
    * Put the port in a env file as well as started with the connecting of the database
    
    * Created the first route
    
    * Fixed error in route
    
    * Added route
    
    * Fixed database connection
    
    * Fixed errors in schemas and used the auth router

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,8 +1,13 @@
 import express from 'express';
+import db from './db.mjs';
+import dotenv from 'dotenv';
+import authRoute from './routes/auth.mjs';
 
+dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.use('/api/auth', authRoute);
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`);
