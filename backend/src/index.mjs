16b21ec74ea375493ff0385c51433dfe92393314
willemharmse <harmse688@gmail.com>
2024-08-27 11:18:39 +0200

    -Added the ability to create a user account
    -Added the ability to hash user password using bcrypt
    
    -Fixed errors in the user schema

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -2,11 +2,13 @@ import express from 'express';
 import db from './db.mjs';
 import dotenv from 'dotenv';
 import authRoute from './routes/auth.mjs';
+import userRoute from './routes/user.mjs';
 
 dotenv.config();
 const app = express();
 
 app.use('/api/auth', authRoute);
+app.use('/api/user', userRoute);
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
