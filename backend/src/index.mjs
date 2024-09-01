72a1b3400ddbcc9e35bcfd641603c3be028acf56
willemharmse <harmse688@gmail.com>
2024-09-01 11:14:23 +0200

    -Added more dependancies
    -Added file upload route
    
    -Added file compression middleware

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -3,12 +3,14 @@ import db from './db.mjs';
 import dotenv from 'dotenv';
 import authRoute from './routes/auth.mjs';
 import userRoute from './routes/user.mjs';
+import fileRoute from '../src/routes/file.mjs';
 
 dotenv.config();
 const app = express();
 
 app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute);
+app.use('/api/file', fileRoute);
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
