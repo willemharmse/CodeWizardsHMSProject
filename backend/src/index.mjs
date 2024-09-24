09f3178b6440d0573116198242ad3df2c2df259e
willemharmse <harmse688@gmail.com>
2024-09-24 12:46:37 +0200

    -Added cors to the backend and fixed issues with the documentation

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import db from './db.mjs';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import index from '../src/routes/index.mjs';
 import { logRequests } from '../src/middleware/logRequests.mjs';
 import { logErrors } from '../src/middleware/logErrors.mjs';
@@ -9,7 +10,7 @@ import statusMonitor from 'express-status-monitor';
 dotenv.config();
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use(statusMonitor());
 
 app.get('/status', statusMonitor().pageRoute);
