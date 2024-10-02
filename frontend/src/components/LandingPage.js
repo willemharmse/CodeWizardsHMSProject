d5d00bc73f8da5309f30e57c4b117321d8ad0d37
zxndre <95588402+zxndre@users.noreply.github.com>
2024-10-02 09:39:30 +0200

    Landing page changes

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -6,24 +6,19 @@ function LandingPage() {
   return (
     <div className="landing-container">
       <header className="landing-header">
-        <h1>Welcome to Our System</h1>
-        <p>Please navigate using the buttons below</p>
+        <div className="header-content">
+          <h2>HMS</h2>
+          <button className="logout-button">Logout</button>
+        </div>
+        <h1>Welcome</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempus varius justo eget faucibus. Duis vestibulum laoreet elit, in blandit justo luctus nec. Nullam et vehicula est. Aenean convallis lobortis tellus, ut tempor arcu gravida sit amet. Proin sit amet neque at velit fringilla tristique non vestibulum mi. Nullam viverra, ante in consectetur viverra, augue odio ultrices diam, eget pharetra felis lectus quis arcu. Phasellus malesuada metus ut neque pellentesque, ut viverra mi tempus. Duis ultrices sit amet sapien nec auctor. Sed eget mauris aliquet, dignissim risus consectetur, dapibus diam.</p>
       </header>
       <div className="button-container">
         <Link to="/login">
-          <button className="landing-button">Login</button>
+          <button className="landing-button">See your courses</button>
         </Link>
         <Link to="/admin">
-          <button className="landing-button">User Administration</button>
-        </Link>
-        <Link to="/assignments">
-          <button className="landing-button">List Assignments</button>
-        </Link>
-        <Link to="/create-assignment">
-          <button className="landing-button">Create Assignment</button>
-        </Link>
-        <Link to="/assignment-videos">
-          <button className="landing-button">Assignment Videos</button>
+          <button className="landing-button">Manage Users</button>
         </Link>
       </div>
     </div>
