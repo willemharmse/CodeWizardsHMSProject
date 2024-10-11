536ab01eb016bbcd5ec65e4bcb2bae5c6176a487
willemharmse <harmse688@gmail.com>
2024-10-11 13:56:07 +0200

    -Clean the branch

--- src/middleware/logErrors.mjs
+++
@@ -1,7 +0,0 @@
-import logger from '../config/logger.mjs';
-
-export const logErrors = (err, req, res, next) => {
-    // Log the error message, stack, and request details
-    logger.error(`Error: ${req.method} ${req.url} - ${err.message}\nStack: ${err.stack}`);
-    res.status(500).send('Internal Server Error');
-};
--- src/middleware/restrictUser.mjs
+++
@@ -1,9 +0,0 @@
-export default function restrictUser(allowedRoles) {
-    return (req, res, next) => {
-      if (!allowedRoles.includes(req.user.role)) {
-        return res.status(403).send('You do not have permission');
-      }
-      next();
-    };
-  }
-  
